c18d577d3922f4787590d8b0f0566c5bc9375092
Eugen Ioan Goriac <eugen.goriac@me.com>
2019-10-18 08:24:34 -0700

    Add TimeKeeper that sends signals to refresh periodically

--- RandomBuddyStalker/MainView.xaml.cs
+++ RandomBuddyStalker/MainView.xaml.cs
@@ -10,7 +10,9 @@ using System.Reactive.Linq;
 namespace ReactiveAvalonia.RandomBuddyStalker {
     public class MainView : ReactiveWindow<MainViewModel> {
         private TextBlock tblBuddyInfo => this.FindControl<TextBlock>("tblBuddyInfo");
+        private TextBlock tblDecisionTimeLeft => this.FindControl<TextBlock>("tblDecisionTimeLeft");
         private Button btnStalkBuddy => this.FindControl<Button>("btnStalkBuddy");
+
 
         public MainView() {
             ViewModel = new MainViewModel();
@@ -28,6 +30,17 @@ namespace ReactiveAvalonia.RandomBuddyStalker {
                                     Console.WriteLine(
                                         $"[v  {Thread.CurrentThread.ManagedThreadId}]: " +
                                         "View deactivated"))
+                            .DisposeWith(disposables);
+                        
+                        // this
+                        //      .OneWayBind(ViewModel, vm => vm.Remaining, v => v.tblDecisionTimeLeft.Text);
+                        this
+                            .WhenAnyValue(v => v.ViewModel.Remaining)
+                            .ObserveOn(RxApp.MainThreadScheduler)
+                            .Subscribe(x => 
+                            {
+                                tblDecisionTimeLeft.Text = x.ToString();
+                            })
                             .DisposeWith(disposables);
                     });
 
--- RandomBuddyStalker/MainViewModel.cs
+++ RandomBuddyStalker/MainViewModel.cs
@@ -5,48 +5,64 @@ using ReactiveUI.Fody.Helpers;
 using Flurl.Http;
 using System.Threading;
 using System.Reactive.Disposables;
+using System.Threading.Tasks;
 
 namespace ReactiveAvalonia.RandomBuddyStalker {
     public class MainViewModel : ReactiveObject, IActivatableViewModel {
         public ViewModelActivator Activator { get; }
-        private readonly long _startTime;
+
+        private static int DecisionTime = 1000;
+
+        private int GetPseudoTimeNowMs() {
+            // 10000000 ms - a bit less than 3 hours
+            return (int)(DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() % 10000000);
+        }
         private int GetTimeSinceStart() {
-            return (int)(DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() - _startTime);
+            return GetPseudoTimeNowMs() - _startTime;
         }
         private int GetThreadId() {
             return Thread.CurrentThread.ManagedThreadId;
         }
 
+        private int _startTime;
+
+        private volatile int _lastDecisionStartTime;
+
+        //private object _locker = new object();
+
+        private int _decisionsLeftCount = 3;
+
+        Random randy = new Random();
+
         // better solution for pausing:
         // https://leeoades.wordpress.com/2012/12/17/pausable-observable/
-        private static volatile bool _isPaused = false;
+        //private static volatile bool _isPaused = false;
+
         public MainViewModel() {
             Activator = new ViewModelActivator();
 
-            _startTime = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
             this.WhenActivated(
                 disposables => {
-                    Console.WriteLine(
-                        $"[vm {GetThreadId()}]: " +
-                        "ViewModel activated");
+                    var timeKeeper = RunTimeKeeperAsync();
+                    Console.WriteLine($"[vm {GetThreadId()}]: ViewModel activated");
 
                     Disposable
                         .Create(
-                            () => 
+                            () => {
+                                _frenzyOn = false;
+                                // TODO: see if I can use a timeout?
+                                timeKeeper.Wait();
                                 Console.WriteLine(
                                     $"[vm {GetThreadId()}]: " +
-                                    "ViewModel deactivated"))
+                                    "ViewModel deactivated");
+                            })
                         .DisposeWith(disposables);
-
+                    
                     // Observable
                     //     .Timer(TimeSpan.Zero, TimeSpan.FromMilliseconds(500))
+                    //     .Where(_ => _isPaused == false)
                     //     .Take(5)
-                    //     //.Select(_ => Observable.FromAsync(async () => await GetRandomUser()))
-                    //     .Select(_ => Observable.FromAsync(async () => await GetRandomUserAsync()))
-                    //     // https://github.com/dotnet/reactive/issues/459
-                    //     // https://blog.jonstodle.com/flattening-observables-in-rx-net/
-                    //     .Concat()
-                    //     //.Merge()
+                    //     .Select(_ => GetRandomUser())
                     //     .ObserveOn(RxApp.MainThreadScheduler)
                     //     .Subscribe(
                     //         x => {
@@ -56,48 +72,89 @@ namespace ReactiveAvalonia.RandomBuddyStalker {
                     //                 $"=> {x}");
                     //         },
                     //         err => Console.WriteLine($"error: {err}"),
-                    //         () => Console.WriteLine("Quota reached... Try later please :) !"))
+                    //         () => Console.WriteLine("Stalking quota reached... Try later please :) !"))
                     //     .DisposeWith(disposables);
+
+
+                    // You, I and ReactiveUI: 14.6 - scheduling
+                    // this
+                    //     .WhenAnyValue(vm => vm.Delta)
+                    //     .Sample(TimeSpan.FromMilliseconds(160))
+                    //     .Select(delta => DecisionTime - delta)
+                    //     .ObserveOn(RxApp.MainThreadScheduler)
+                    //     //.BindTo(this, vm => vm.Remaining);
+                    //     .Subscribe(x => {
+                    //         Remaining = x;
+                    //         System.Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}!{x}");
+                    //     })
+                    //     .DisposeWith(disposables);
+                        
+                        //.ObserveOn(RxApp.MainThreadScheduler)
+                        // .Do(delta => {
+                        //     //Remaining = Math.Max(0, DecisionTime - delta).ToString();
+                        //     var rem = Math.Max(0, DecisionTime - delta).ToString();
+                        //     System.Console.WriteLine($"rem: {rem}");
+                        //     var xx = randy.Next() % 4 + delta;
+                        //     Remaining = xx;
+                        //     //Remaining = rem;
+                        // })
+                        // .Subscribe();
                     
-                    Observable
-                        .Timer(TimeSpan.Zero, TimeSpan.FromMilliseconds(500))
-                        .Where(_ => _isPaused == false)
-                        .Take(5)
-                        .Select(_ => GetRandomUser())
-                        .ObserveOn(RxApp.MainThreadScheduler)
-                        .Subscribe(
-                            x => {
-                                Console.WriteLine(
-                                    $"[sb {GetThreadId()}]: " +
-                                    $"|{GetTimeSinceStart()}| " +
-                                    $"=> {x}");
-                            },
-                            err => Console.WriteLine($"error: {err}"),
-                            () => Console.WriteLine("Stalking quota reached... Try later please :) !"))
-                        .DisposeWith(disposables);
+                    // this
+                    //     .WhenAnyValue(vm => vm.Delta)
+                    //     .Do(
+                    //         x => {
+                    //             lock (_locker) {
+                    //                     System.Console.WriteLine($"Left: {_decisionsLeftCount}");
+                    //                     if (_decisionsLeftCount == 0)
+                    //                         _frenzyOn = false;
+                    //                 }
+                    //             }
+
+                    //             System.Console.WriteLine($"deltuta: {x,5}");
+                    //         }
+                    //     )
+                    //     .Subscribe();
                 });
+            Remaining = 123;
         }
 
         Random _randomizer = new Random();
 
         [Reactive]
+        public int Remaining { get; set; }
+
+        [Reactive]
         public string BuddyName { get; set; }
 
-        // https://blog.jonstodle.com/task-toobservable-observable-fromasync-task/
-        // private async Task<string> GetRandomUserAsync() {
-        //     System.Console.WriteLine(
-        //         $"[ga>{GetThreadId()}]: " +
-        //         $"|{GetTimeSinceStart()}| => Asking for string");
-        //     // Last time I checked there were 12 users with id's from [1..12]
-        //     int userId = _randomizer.Next() % 12 + 1;
-        //     string getResp = await $"https://reqres.in/api/users/{userId}".GetStringAsync();
-        //     getResp = getResp.Substring(13, 2);
-        //     //Thread.Sleep(2000);
-        //     System.Console.WriteLine(
-        //         $"[ga<{GetThreadId()}]: " +
-        //         $"|{GetTimeSinceStart()}| => String received = {getResp}");
-        //     return getResp;
-        // }
+        [Reactive]
+        public int Delta { get; set; }
+
+        private bool _frenzyOn = true;
+        private async Task RunTimeKeeperAsync() {
+            await Observable.Start(() => {
+                _startTime = GetPseudoTimeNowMs();
+                _lastDecisionStartTime = GetPseudoTimeNowMs();
+                while (_frenzyOn) {
+                    //lock (_locker) {
+                        int newDelta = GetPseudoTimeNowMs() - _lastDecisionStartTime;
+
+                        if (newDelta > DecisionTime) {
+                            _decisionsLeftCount--;
+                            System.Console.WriteLine("@@@@" + _decisionsLeftCount);
+                            if (_decisionsLeftCount == 0)
+                                break;
+
+                            _lastDecisionStartTime += DecisionTime;
+                            newDelta -= DecisionTime;
+                        }
+
+                        Delta = newDelta;
+                    //}
+                    Thread.Sleep(30);
+                }
+            }, RxApp.TaskpoolScheduler);
+        }
 
         private string GetRandomUser() {
             System.Console.WriteLine(
